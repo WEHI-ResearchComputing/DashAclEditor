3d8072ec26451169f3551cfeb85bbbc41e73d594
Michael Milton <ttmigueltt@gmail.com>
2024-05-01 12:54:28 +1000

    startup logging

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,6 +1,9 @@
 import json
 import sys
 import os.path
+import logging
+
+logger = logging.getLogger(__name__)
 
 # Automatically enforce running this app with the configured interpreter
 with open("config.json") as fp:
@@ -10,8 +13,14 @@ with open("config.json") as fp:
     else:
         target_interpreter = os.path.normpath(target_interpreter)
 
-if os.path.normpath(sys.executable) != target_interpreter:
+current_interpreter = os.path.normpath(sys.executable)
+logger.info(target_interpreter + " is the chosen interpreter")
+logger.info(current_interpreter + " is the current interpreter")
+
+if current_interpreter != target_interpreter:
+    logger.info("Restarting with chosen interpreter")
     os.execl(target_interpreter, target_interpreter, *sys.argv)
 else:
     from acledit.app import app
     application = app.server
+    logger.info("wsgi application successfully found")
